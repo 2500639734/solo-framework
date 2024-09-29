ea1a15add60117c7df84b397ab2a14f657b333a0
shenke <2500639734@qq.com>
2024-09-30 04:21:30 +0800

    feat(web): swagger配置优化

--- solo-framework-starter-core/src/main/java/com/solo/framework/core/configuration/SoloFrameworkCoreAutoConfiguration.java
+++ solo-framework-starter-core/src/main/java/com/solo/framework/core/configuration/SoloFrameworkCoreAutoConfiguration.java
@@ -1,10 +1,87 @@
 package com.solo.framework.core.configuration;
 
+import cn.hutool.core.collection.CollUtil;
+import com.solo.framework.core.context.SoloFrameworkContextHolder;
+import com.solo.framework.core.env.SoloFrameworkRuntimeInfo;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class SoloFrameworkCoreAutoConfiguration implements Ordered {
+
+    @Bean
+    public SoloFrameworkRuntimeInfo soloFrameworkRuntimeInfo() {
+        return new SoloFrameworkRuntimeInfo().setBasePackages(getApplicationScanBasePackages(SoloFrameworkContextHolder.getApplicationContext()));
+    }
+
+    /**
+     * 获取当前应用的扫描包路径
+     * @param applicationContext 容器上下文
+     * @return 当前应用的扫描包路径
+     */
+    private Set<String> getApplicationScanBasePackages(ApplicationContext applicationContext) {
+        // 默认
+        if (Objects.isNull(applicationContext)) {
+            return CollUtil.newHashSet("/");
+        }
+
+        // 优先取@ComponentScan
+        Set<String> scanBasePackages = getComponentScanBasePackages(applicationContext);
+        if (CollUtil.isNotEmpty(scanBasePackages)) {
+            return scanBasePackages;
+        }
+
+        // 如果未标注@ComponentScan，取标注@SpringBootApplication的包路径
+        scanBasePackages = getApplicationDefaultScanBasePackages(applicationContext);
+        return scanBasePackages;
+    }
+
+    /**
+     * 获取当前应用的扫描包路径(基于@ComponentScan)
+     * @param applicationContext 容器上下文
+     * @return 当前应用的扫描包路径
+     */
+    private Set<String> getComponentScanBasePackages(ApplicationContext applicationContext) {
+        return applicationContext
+                .getBeansWithAnnotation(ComponentScan.class).values().stream()
+                .map(bean -> AnnotatedElementUtils.findMergedAnnotationAttributes(bean.getClass(), ComponentScan.class, false, true))
+                .filter(Objects::nonNull)
+                .flatMap(attributes -> Stream.of(attributes.getStringArray("basePackages")))
+                .filter(basePackage -> ! basePackage.contains("springfox") && ! basePackage.contains("com.github"))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * 获取当前应用的扫描包路径(基于@SpringBootApplication(未指定scanBasePackages时))
+     * @param applicationContext 容器上下文
+     * @return 当前应用的扫描包路径
+     */
+    private Set<String> getApplicationDefaultScanBasePackages(ApplicationContext applicationContext) {
+        return Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(beanDefinitionName -> {
+                    try {
+                        Object bean = applicationContext.getBean(beanDefinitionName);
+                        Class<?> clazz = bean.getClass();
+                        if (clazz.getAnnotation(SpringBootApplication.class) != null) {
+                            return clazz.getPackage().getName();
+                        }
+                    } catch (Exception e) {
+                        // skip 这里仅为了获取默认的scanBasePackages, 可能会报错循环依赖, 不会影响框架启动, 忽略这个异常
+                    }
+                    return null;
+                }).filter(Objects::nonNull).collect(Collectors.toSet());
+    }
 
     @Override
     public int getOrder() {
--- solo-framework-starter-web/src/main/java/com/solo/framework/web/configuration/web/swagger/SoloFrameworkWebSwaggerAutoConfiguration.java
+++ solo-framework-starter-web/src/main/java/com/solo/framework/web/configuration/web/swagger/SoloFrameworkWebSwaggerAutoConfiguration.java
@@ -3,22 +3,17 @@ package com.solo.framework.web.configuration.web.swagger;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.solo.framework.core.constant.SoloFrameworkPropertiesPrefixConstant;
-import com.solo.framework.core.context.SoloFrameworkContextHolder;
 import com.solo.framework.core.env.SoloFrameworkRuntimeInfo;
 import com.solo.framework.core.properties.web.swagger.SoloFrameworkWebSwaggerProperties;
 import com.solo.framework.core.properties.web.swagger.concat.SoloFrameworkWebSwaggerConcatProperties;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.AnnotatedElementUtils;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -28,12 +23,8 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 @Slf4j
@@ -47,19 +38,18 @@ public class SoloFrameworkWebSwaggerAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(Docket.class)
-    public Docket soloFrameworkSwaggerDocket() {
+    public Docket soloFrameworkSwaggerDocket(SoloFrameworkRuntimeInfo soloFrameworkRuntimeInfo) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo(soloFrameworkWebSwaggerProperties))
                 .enable(true)
                 .select()
-                .apis(requestHandlerPredicate())
+                .apis(requestHandlerPredicate(soloFrameworkRuntimeInfo))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private Predicate<RequestHandler> requestHandlerPredicate() {
-        Set<String> scanBasePackages = CollUtil.defaultIfEmpty(soloFrameworkWebSwaggerProperties.getBasePackages(), getApplicationScanBasePackages(SoloFrameworkContextHolder.getApplicationContext()));
-        log.info("==============>>>scanBasePackages: {}", scanBasePackages);
+    private Predicate<RequestHandler> requestHandlerPredicate(SoloFrameworkRuntimeInfo soloFrameworkRuntimeInfo) {
+        Set<String> scanBasePackages = CollUtil.defaultIfEmpty(soloFrameworkWebSwaggerProperties.getBasePackages(), soloFrameworkRuntimeInfo.getBasePackages());
         return scanBasePackages
                 .stream()
                 .map(RequestHandlerSelectors::basePackage)
@@ -76,46 +66,6 @@ public class SoloFrameworkWebSwaggerAutoConfiguration {
                 .contact(new Contact(concat.getName(), concat.getUrl(), concat.getEmail()))
                 .version(StrUtil.blankToDefault(soloFrameworkWebSwaggerProperties.getVersion(), SoloFrameworkRuntimeInfo.VERSION))
                 .build();
-    }
-
-    private Set<String> getApplicationScanBasePackages(ApplicationContext applicationContext) {
-        if (Objects.isNull(applicationContext)) {
-            return CollUtil.newHashSet("/");
-        }
-
-        Set<String> scanBasePackages = getComponentScanBasePackages(applicationContext);
-        if (CollUtil.isNotEmpty(scanBasePackages)) {
-            return scanBasePackages;
-        }
-
-        scanBasePackages = getApplicationDefaultScanBasePackages(applicationContext);
-        return scanBasePackages;
-    }
-
-    private Set<String> getComponentScanBasePackages(ApplicationContext applicationContext) {
-        return applicationContext
-                .getBeansWithAnnotation(ComponentScan.class).values().stream()
-                .map(bean -> AnnotatedElementUtils.findMergedAnnotationAttributes(bean.getClass(), ComponentScan.class, false, true))
-                .filter(Objects::nonNull)
-                .flatMap(attributes -> Stream.of(attributes.getStringArray("basePackages")))
-                .filter(basePackage -> ! basePackage.contains("springfox") && ! basePackage.contains("com.github"))
-                .collect(Collectors.toSet());
-    }
-
-    private Set<String> getApplicationDefaultScanBasePackages(ApplicationContext applicationContext) {
-        return Arrays.stream(applicationContext.getBeanDefinitionNames())
-                .map(beanDefinitionName -> {
-                    try {
-                        Object bean = applicationContext.getBean(beanDefinitionName);
-                        Class<?> clazz = bean.getClass();
-                        if (clazz.getAnnotation(SpringBootApplication.class) != null) {
-                            return clazz.getPackage().getName();
-                        }
-                    } catch (Exception e) {
-                        // skip 这里仅为了获取默认的scanBasePackages, 可能会报错循环依赖, 不会影响框架启动, 忽略这个异常
-                    }
-                    return null;
-                }).filter(Objects::nonNull).collect(Collectors.toSet());
     }
 
 }
